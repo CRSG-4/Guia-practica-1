a2cb7348424ec18cf518492e8757922bdc6ff128
CRSG-4 <cgaldamezrenderos03@gmail.com>
2025-08-21 20:31:01 -0600

    Add files via upload

---
+++ ilcm.java
@@ -0,0 +1,21 @@
+/*
+Ivan Lizandro Castellanos Martinez
+2502662025
+--------------------
+Cristian Alexander Galdámez Renderos
+2500662025
+
+--------------------
+Brayan Orlando Cabezas Moreno 
+2504622025
+--------------------
+Emerson Eliseo Flamenco Hernandez 
+2521372025
+ */
+class ilcm
+{
+	public static void main (String liz[])
+	{
+		System.out.println("clase creada con iniciales del nombre y apellidos");
+	}
+}
